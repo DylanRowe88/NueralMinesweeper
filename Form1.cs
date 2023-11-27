e08c11647da6aeb77b17c5db13ed7f4216536329
DylanRowe88 <98070240+DylanRowe88@users.noreply.github.com>
2023-11-26 23:33:49 -0500

    Update

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,60 @@
+using Microsoft.Toolkit.Uwp.Notifications; // If we want to do windows toast notifs
+
+using System.Drawing.Drawing2D;
+using System.Xml.Linq;
+
 namespace NueralMinesweeper
 {
     public partial class Form1 : Form
     {
+        private readonly Minesweeper myMinesweeper;
+        List<UIMine> uiMineList = new();
+        // Mine Graphics
+        int sideLen = 50; // Field is made of squares...
+        double xscale = 5;
+        double yscale = 7;
+
         public Form1()
         {
             InitializeComponent();
+            WinNotif("Test");
+            myMinesweeper = new(10,10,20);
+        }
+        // Function I made to create windows notifications, unused for now
+        void WinNotif(String notifText)
+        {
+            new ToastContentBuilder()
+                .AddArgument("action", "viewConversation")
+                .AddArgument("conversationId", 9813)
+                .AddText("AI Proj")
+                .AddText(notifText);
+        }
+    }
+    // Created class inheriting Button to customize its shape
+    class UIMine : Button
+    {
+        public UIMine(int SideLen)
+        {
+            this.Text = "";
+            this.Font = new Font("Arial", 12, FontStyle.Regular);
+            this.Height = SideLen;
+            this.Width = SideLen;
+        }
+
+        // public Mine Mine { set; get; }
+        public void setMineVal(int     val) { this.Text = val.ToString(); }
+        public void setMineVal(string text) { this.Text = text; } // For marking flags and bombs
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            GraphicsPath path = new GraphicsPath();
+            // path.AddEllipse(
+            //    ClientRectangle.X + ClientRectangle.Width / 4,
+            //    ClientRectangle.Y + ClientRectangle.Height / 4,
+            //    ClientRectangle.Width / 2,
+            //    ClientRectangle.Height / 2);
+            Region = new(path);
+            base.OnPaint(e);
         }
     }
 }
---
+++ Minesweeper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NueralMinesweeper
+{
+    public class Minefield : IComparable // Class to keep track of edges with consolidated methods
+    {
+        private class Mine
+        {
+            public Mine() { mineVal = 0; X = -1; Y = -1; } // Null Node
+            public Mine(int newMineVal, int newX, int newY)
+            {
+                mineVal = newMineVal; X = newX; Y = newY;
+            }
+
+            public bool isMine;
+            public bool isCovered;
+            public int mineVal;
+            public double X, Y;
+            public override string ToString() => $"({X}, {Y})";
+        }
+
+        public readonly int width, height;
+        public readonly int mineCount;    // How many mines are in field
+        public readonly int uncoveredCount; // How many tiles are covered
+        public readonly int moveCount;    // How many moves have been made on field
+        public readonly int boomCount;    // How many mines have been hit
+
+        private List<Mine> field = new();
+        private Random rng = new Random();
+
+        public Minefield(List<int> Minefield) // Input a pre-generated minefield
+        {
+            foreach (int val in Minefield)
+            {
+                field.Add(val);
+            }
+        }
+        public Minefield(int width, int height, int minecount) // Create minefield with dimensions and minecount
+        {
+            field = nodes; // Defined Starting point
+            length = pathLength;
+        }
+        public double RatioUncovered()
+        {
+            return uncoveredCount/field.Count;
+        }
+        public int CompareTo(object? obj)
+        {
+            if (obj is Minefield T)
+            {
+                if (T.RatioUncovered() < RatioUncovered()) { return 1; }
+                else if (T.RatioUncovered() > RatioUncovered()) { return -1; }
+                else { return 0; }
+            }
+            return 1;
+        }
+
+    }
+    // internal class Minesweeper // This was here at first, idk what internal does
+    class Minesweeper
+    {
+        public Minefield Field;
+        public Minesweeper(int width, int height, int mineCount) { Field = new(width, height, mineCount); }
+
+        //public readonly List<List<Minefield>> Generations = new();
+        //public readonly List<Double> BestFitperGeneration = new();
+
+    }
